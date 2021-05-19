c710eb40e9016684002b4491b8ccd184d8a6131a
Collin Lanier <collinlanier12@gmail.com>
2021-05-19 01:58:49 -0400

    mobile first header complete

---
+++ frontend/src/components/Header/Header.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { AppBar, makeStyles, Toolbar, Typography } from '@material-ui/core';
+
+const useStyles = makeStyles(() => ({
+    header: {
+        backgroundColor: "#4896B8",
+    },
+    logo: {
+        fontFamily: "Nunito",
+        fontWeight: 600,
+        color: "#FFFFF",
+        textAlign: "left",
+    },
+}));
+
+const Header = () => {
+    const { header, logo } = useStyles();
+    return (
+        <header>
+            <AppBar className={header}>
+                <Toolbar>
+                    <Typography variant="h6" component="h1" className={logo}>Anime Watchlist</Typography>
+                </Toolbar>
+            </AppBar>
+        </header>
+
+    );
+}
+
+export default Header;
