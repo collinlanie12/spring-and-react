683f099d54ac0b65c562f16dcb98fca1c0cf3ace
Collin Lanier <collinlanier12@gmail.com>
2021-05-19 00:48:06 -0400

    deletion of styled-components, addition of material-ui

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,11 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
-import GlobalStyles from './GlobalStyles';
 
 import Home from './pages/Home/Home';
 
 const App = () => {
   return (
     <div className="App">
-      <GlobalStyles />
       <div>
         <Switch>
           <Route exact path='/' component={Home} />
